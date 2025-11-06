f9aae8d14a8bba1d65d7e27f6fc62257c4052297
Stan-breaks <stanleymwenwda03@gmail.com>
2025-11-06 20:17:54 +0300

    feat: simple comments

--- Main.java
+++ Main.java
@@ -2,7 +2,10 @@ import java.util.Scanner;
 
 public class Main {
   public static void main(String[]args) {
+    //getting the input from the stdin
     Scanner input = new Scanner(System.in);
+
+    //printing to the stdout
     System.out.println("=== Simple Java CLI ===");
     System.out.print("Enter your name: ");
     String name = input.nextLine();
